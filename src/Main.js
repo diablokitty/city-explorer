40e6f4b4c7ca9a14264d36deb775ea7608dd229b
Amy Pierce <diablokitty@gmail.com>
2022-08-23 12:29:07 -0700

    finished all lab requirements

--- src/Main.js
+++ src/Main.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import './Main.css';
+
 
 class Main extends React.Component {
   constructor(props) {
@@ -11,7 +13,8 @@ class Main extends React.Component {
       city: '',
       cityData: {},
       error: false,
-      errorMessage: ''
+      errorMessage: '',
+      display: false
     }
   }
 
@@ -25,14 +28,25 @@ class Main extends React.Component {
   handleSubmit = async (e) => {
     e.preventDefault();
     // API request through axios and save in state
-    let response = await axios.get(`https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`);
-    console.log(response.data[0]);
-    this.setState({
-      cityData: response.data[0]
-    })
-    console.log(this.state.cityData.lon);
+    try {
+      let response = await axios.get(`https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`);
+      console.log(response.data[0]);
+      this.setState({
+        cityData: response.data[0],
+        error: false,
+        display: true
+      });
 
-  }
+    } catch (error) {
+      console.log('error: ', error);
+      console.log('error.message: ', error.message);
+      this.setState({
+        error: true,
+        errorMessage: `Something's not right here - ${error.response.status}`
+      });
+    }
+    console.log(this.state.errorMessage);
+  };
 
   render() {
 
@@ -40,45 +54,53 @@ class Main extends React.Component {
       <p>{this.state.cityData.lat}{this.state.cityData.lon}</p>
     </section>
 
+    //let viewMap = <img src = "https://maps.locationiq.com/v3/staticmap?key={process.env.REACT_APP_LOCATIONIQ_API_KEY}&center={this.state.cityData.lat},{this.state.cityData.lon}&zoom=10>" ,"
 
 
     return (
       <>
+      <main>
         <h1>City Explorer</h1>
         <Form onSubmit={this.handleSubmit}>
-          <Form.Label htmlFor="city">Pick a City</Form.Label>
+          <Form.Label htmlFor="city">Pick a City or Location</Form.Label>
           <Form.Control
             type="text"
             id="city"
             onInput={this.handleInput}
           />
 
-          <Button variant="primary" type="submit" >
+          <Button variant="outline-light"type="submit" >
             Explore!
           </Button>
         </Form>
 
-
-
-        <Card style={{ width: '18rem' }}>
-          <Card.Body>
-            <Card.Title>Oh, The Places You'll Go!</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">Your Destination</Card.Subtitle>
-            <Card.Text>
-              {locCoord}
-            </Card.Text>
-          </Card.Body>
-        </Card>
-
+        {
+          this.state.error
+            ?
+            <p>{this.state.errorMessage}</p>
+            :
+            this.state.display
+            ?
+            <Card bg='dark' text='light' style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title>Oh, The Places You'll Go!</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Your Destination</Card.Subtitle>
+                <Card.Img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=14`} />
+                <Card.Text>
+                  {locCoord}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            :
+            <Card.Text>Where are we going?</Card.Text>
+        }
+        </main>
       </>
     );
-
-
-
-
 
   }
 }
 
 
 export default Main;
+
